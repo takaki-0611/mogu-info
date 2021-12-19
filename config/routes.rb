8b5eeda79c982265428f55af4d16e62642e2bce0
takaki-0611 <pq.earlyripe.earlyrotten@gmail.com>
2021-12-19 10:25:35 +0000

    ルーティングno

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,17 @@
 Rails.application.routes.draw do
+  get '/search' => 'search#search'
   devise_for :users
-  get 'comments/create'
-  get 'comments/desrtroy'
-  get 'favorites/create'
-  get 'favorites/destroy'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
-  get 'homes/top'
-  get 'homes/about'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/quit'
-  get 'users/out'
+  root 'homes#top'
+  get '/about' => 'homes#about'
+  resources :users,only: [:show, :index, :edit, :update] do
+    collection do
+      get 'quit'
+      patch 'out'
+    end
+  end
+  resources :posts, only: [:index, :show, :create, :edit, :update, :destroy] do
+    resources :comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
